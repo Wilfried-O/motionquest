e8dfd324d622acfcff6a45a600140ae090b56ed7
Wilfried Okou <will.okson@gmail.com>
2025-10-15 00:22:59 -0400

    Initial cleanup and setup: remove default React files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>MotionQuest</h1>
+            <h3>Movie Search app</h3>
+            <p>Work in progress...</p>
+        </div>
+    );
+}
+
+export default App;
